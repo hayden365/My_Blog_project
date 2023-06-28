dd2af98d33ba5015cc7525047c4c1a7df0548896
hayden365 <ysmh100@gmail.com>
2023-06-28 15:48:37 +0900

    types module

--- src/components/post-detail/PostContent.tsx
+++ src/components/post-detail/PostContent.tsx
@@ -5,6 +5,7 @@ import formatDate from '@/utils/formatDate';
 import { IPost, IPostDetail } from '../../../type';
 import Image from 'next/image';
 import usePost from '@/hooks/usePost';
+import BubbleIcon from '../icons/BubbleIcon';
 
 type Props = {
 	postId: string;
@@ -29,9 +30,19 @@ export default function PostContent({ postId }: Props) {
 			</div>
 			{post && (
 				<section className="flex flex-col p-4 pt-5 gap-3 mb-20">
-					<h1 className="text-4xl font-bold">{post?.title}</h1>
-					<span>{formatDate(post.createdAt)}</span>
-					<div className="w-44 border-2 border-sky-600 mt-4 mb-8" />
+					<h1 className="text-center text-4xl font-bold text-primary px-10">
+						{post?.title}
+					</h1>
+					<div className="flex justify-center gap-5 font-serif text-uFontColor">
+						<div className="flex items-center gap-1">
+							<span> {post.comments.length}</span>
+							<BubbleIcon style="text-uRed relative -top-[2px]" />
+						</div>
+						<div className="w-7 h-[2px] bg-gray-200 self-center" />
+
+						<span>{formatDate(post.createdAt)}</span>
+					</div>
+					<div className="w-full border-2 border-gray-100 mt-4 mb-8" />
 					<MarkdownViewer content={post.content} />
 				</section>
 			)}
